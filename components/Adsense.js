19e9c0bb39555e3ac6fece09e47cc1789af8eb44
Shreyash Pawar <87687490+greedytaker@users.noreply.github.com>
2022-06-26 20:14:36 +0530

    Update Adsense.js

--- components/Adsense.js
+++ components/Adsense.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react"
+export default function Adsense(...props) {
+    const { currentPath } = props;
+    useEffect(() => {
+        window.adsbygoogle = window.adsbygoogle || []
+        window.adsbygoogle.push({})
+      }, [currentPath])
 
-const Adsense = () => {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-
-  return (
-<ins className="adsbygoogle"
+    return(
+        <div key={currentPath}>
+        { /*START horizonalAds Google Adsense */ }
+          <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-9684334826790570"
      data-ad-slot="2122721910"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
-  );
-};
+      
 
-export default Adsense;
+        </div>
+    )
+}
