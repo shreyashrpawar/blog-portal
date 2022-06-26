addca3d10cb2256ad09726c84fc83a4ee55f913f
Shreyash Pawar <87687490+greedytaker@users.noreply.github.com>
2022-06-26 19:25:52 +0530

    Create Adsense.js

---
+++ components/Adsense.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+export default function Adsense() {
+  const loadAds = () => {
+    try {
+      if (typeof window !== "undefined") {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (error) {
+      console.log("adsense error", error.message);
+    }
+  };
+
+  useEffect(() => {
+    loadAds();
+  }, []);
+
+  return (
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9684334826790570"
+     data-ad-slot="2122721910"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+  );
+}
