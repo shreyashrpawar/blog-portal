1ce1fc20b9b8709d67c579634e5d3a7c6f095f7c
Shreyash Pawar <87687490+greedytaker@users.noreply.github.com>
2022-06-27 08:05:44 +0530

    Create _adsense.js

---
+++ pages/_adsense.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+
+export default function Adsense() {
+  const loadAds = () => {
+    try {
+      if (typeof window !== "undefined") {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    } catch (error) {
+      console.log("adsense error", error.message);
+    }
+  };
+
+  useEffect(() => {
+    loadAds();
+  }, []);
+
+  return (
+<amp-ad width="100vw" height="320"
+     type="adsense"
+     data-ad-client="ca-pub-9684334826790570"
+     data-ad-slot="2122721910"
+     data-auto-format="rspv"
+     data-full-width="">
+  <div overflow=""></div>
+</amp-ad>
+  );
+}
