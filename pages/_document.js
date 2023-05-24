fd2a7a0c4f4e5bfc25de770ff222bb86d39273ed
Shreyashpawar28 <shreyashpawar0000@gmail.com>
2023-05-24 17:20:07 +0530

    decrease time

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="theme-compiled">
        <Head>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9684334826790570"
-     crossOrigin="anonymous"></script>
             <link rel="icon" type="image/x-icon" href="/favicon.ico" />
   <link rel="apple-touch-icon" href="/favicon.ico"></link>
-  <meta name="google-site-verification" content="ZycfOA0TWx-56phJFWCR7JmWkdSR8AOa_qKYjs7doEk" />
        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
